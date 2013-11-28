0fb33a5f3adbcce4ee259bf9913f708b2507f3a7
tanmay <tsongade@gmail.com>
2013-11-28 12:14:41 +0530

    refactoring code related to css

--- formbuilder.js
+++ formbuilder.js
@@ -1356,7 +1356,7 @@ __p += '\n  method="post">\n';
  } else { ;
 __p += '\n<div class=\'fb-right\'>\n';
  } ;
-__p += '\n  <div class=\'fb-no-response-fields\'>\n    <div class=\'input-line nav_help_user\'>\n      <img src="/assets/nav_help_user_new_form.png">\n      <label>Select fields to Add</label>\n    </div>\n  </div>\n  <div class=\'fb-response-fields\'></div>\n  ';
+__p += '\n  <div class=\'fb-no-response-fields\'>\n    <div class=\'input-line nav_help_user\'>\n      <div class=\'nav_help_user_new_form\'></div>\n      <label>Select fields to Add</label>\n    </div>\n  </div>\n  <div class=\'fb-response-fields\'></div>\n  ';
  if(opts && opts.submitUrl) { ;
 __p += '\n  <input type="submit" value="Submit">\n  ';
  } ;
