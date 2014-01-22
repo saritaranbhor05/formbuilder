d49aec53be6b3066418f5798bc4a3bc12f729ed5
tanmay <tsongade@gmail.com>
2014-01-22 13:22:34 +0530

    Added proper css

--- formbuilder.js
+++ formbuilder.js
@@ -1478,7 +1478,7 @@
 
 (function() {
   Formbuilder.registerField('gmap', {
-    view: "<input type='button' style=\"width: 100px ;height: 35px;padding-top: 5px;padding-bottom: 5px;\" id=\"gmap_button\" value=\"\" />",
+    view: "<input type='button' style=\"min-width: 100px ;height: 35px;padding-top: 5px;padding-bottom: 5px;\" id=\"gmap_button\" value=\"\" />",
     edit: "",
     addButton: "<span class=\"symbol\"><span class=\"icon-map-marker\"></span></span> google maps",
     isValid: function($el, model) {
