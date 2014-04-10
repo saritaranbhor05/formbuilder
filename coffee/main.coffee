42d7a9659fd8c8203ed632271a686772973a1b28
kavita <kavita@vertisinfotech.com>
2014-04-10 17:46:02 +0530

    conditional rendering with section break in easywizard.

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -248,10 +248,9 @@ class Formbuilder
         $(".fb-tab.step.active .fb-field-wrapper:visible").each ->
           outerHeight += $(this).height()
 
-        $('.easyWizardButtons').css('position', 'absolute');
+        $('.easyWizardButtons').css('position', 'static');
         $('.easyWizardButtons').css('top',outerHeight);
         $('.easyWizardButtons').css('width',$('.easyPager').width()-20);
-        $('#formbuilder_form').css('height', outerHeight+50);
         return @
 
       evalCondition: (clicked_element, source_model, condition, value)->
--- formbuilder.js
+++ formbuilder.js
@@ -337,10 +337,9 @@
           $(".fb-tab.step.active .fb-field-wrapper:visible").each(function() {
             return outerHeight += $(this).height();
           });
-          $('.easyWizardButtons').css('position', 'absolute');
+          $('.easyWizardButtons').css('position', 'static');
           $('.easyWizardButtons').css('top', outerHeight);
           $('.easyWizardButtons').css('width', $('.easyPager').width() - 20);
-          $('#formbuilder_form').css('height', outerHeight + 50);
           return this;
         },
         evalCondition: function(clicked_element, source_model, condition, value) {
